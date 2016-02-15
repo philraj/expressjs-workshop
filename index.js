478a5e774d206e573d6f2b1863a051211f8d6ce3
Phil Rajchgot <tophil@outlook.com>
2016-02-15 20:38:41 +0000

    created /contents endpoint which retrieves subset of db info using sequelize query

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var util = require('util');
 var app = express();
 
+//exercise 1
 app.get('/hello', function (req, res) {
   res.send('<html><body><h1>Hello World!</h1></body></html>');
 });
 
-/*Create a web server that can listen to requests for /hello/:firstName, and respond with some HTML that says <h1>Hello _name_!</h1>. For example, if a client requests /hello/John, the server should respond with <h1>Hello John!</h1>*/
+//exercise 2
 app.get('/hello/:firstName', function (req, res) {
   res.send("<h1>Hello " + req.params.firstName + "!</h1>");
 });
@@ -38,6 +40,111 @@ app.get('/op/:operation/:number1/:number2', function(req, res) {
   
   res.json(result);
 });
+
+
+//////////////
+//exercise 4//
+//////////////
+var Sequelize = require("sequelize");
+
+var db = new Sequelize('reddit_clone', 'philraj', null, {
+  dialect: 'mysql'
+});
+
+var User = db.define('user', {
+  username: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING // TODO: make the passwords more secure!
+});
+
+var Content = db.define('content', {
+  url: Sequelize.STRING,
+  title: Sequelize.STRING
+});
+
+var Vote = db.define('vote', {
+  upVote: Sequelize.BOOLEAN
+});
+
+Content.belongsTo(User); // This will add a `setUser` function on content objects
+User.hasMany(Content); // This will add an `addContent` function on user objects
+
+User.belongsToMany(Content, {through: Vote, as: 'upVotes'}); // This will add an `add`
+Content.belongsToMany(User, {through: Vote});
+
+
+
+app.get('/contents', function(req, res) {
+  //run query
+  Content.findAll({
+    include: [{
+      model: User,
+      where: {id: Sequelize.col('content.userId')}
+    }],
+    order: [['createdAt', 'desc']],
+    limit: 5
+  }).then( function (results) {
+    var cleaned = results.map( function (result) {
+      var obj = result.dataValues;
+      obj.user = obj.user.dataValues;
+      
+      return obj;
+    })
+    
+    console.log(util.inspect(cleaned, {colors: true, depth: null}));
+    //now the cleaned up result is ready
+    var html = 
+    `<div id="contents">
+      <h1>List of contents</h1>
+      <ul class="contents-list">`;
+      
+    cleaned.forEach( function (content) {
+      console.log(util.inspect(content, {colors: true, depth: null}));
+
+      html += `<li class="content-item">
+        <h2 class="content-item__title">
+          <a href="` + content.url + `">` + content.title + `</a>
+        </h2>
+        <p>Created by` + content.user.username + `</p>
+      </li>`
+    })
+        
+    html += `</ul>
+          </div>`
+          
+    res.send(html);
+  })
+});
+
+function createNewUser (name, pass, email) {
+  return User.create({
+    username: name,
+    password: pass,
+    email: email
+  })
+}
+
+function createNewContent (userID, url, title) {
+  return User.findById(userID)
+  .then( function(user) {
+    return user.createContent({
+      url: url,
+      title: title
+    })
+  })
+}
+
+function voteOnContent (contentID, userID, isUpVote) {
+  return Promise.all([
+    User.findById(userID),
+    Content.findById(contentID)
+  ])
+  .then( function (val) {
+    val[0].addUpVotes(val[1], {
+      upVote: isUpVote
+    })
+  })
+}
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
